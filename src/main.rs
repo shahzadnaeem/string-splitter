02fba7e83af46c61107ca942295fd195ea8a0b7d
Shahzad Naeem <shahzad65naeem@gmail.com>
2023-02-03 18:47:36 +0000

    Options support

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,35 @@
 use std::env;
 
+use getopts::Options;
+
 mod splitter;
 
 fn main() {
-    let mut input: String = String::from("Hello😊 😊😊😊🐡🐡🐡 😊😊😊🐡 ");
+    let mut opts = Options::new();
+    opts.optflag("u", "until", "Until ...");
+
+    let _args: Vec<String> = env::args().collect();
+    let matches = match opts.parse(&_args[1..]) {
+        Ok(m) => m,
+        Err(fail) => panic!("{}", fail.to_string()),
+    };
+
+    let mut input: String = String::from("Hello😊 😊🐡😊😊🐡🐡🐡 😊😊😊🐡 ");
     let mut delimiter = '🐡';
 
-    if env::args().len() >= 3 {
-        // We have been give some args to play with!
-        let args: Vec<String> = env::args().collect();
+    if matches.free.len() >= 2 {
+        input = matches.free[0].clone();
 
-        input = args[1].clone();
-
-        if args[2].len() > 0 {
-            delimiter = args[2].chars().nth(0).unwrap();
+        if matches.free[1].len() > 0 {
+            delimiter = matches.free[1].chars().nth(0).unwrap();
         }
     }
 
-    let res: Vec<&str> = splitter::StrSplit::new(&input, delimiter).collect();
-
-    println!("{:?}", res);
+    if matches.opt_present("u") {
+        let res = splitter::until_char(&input, delimiter);
+        println!("{:?}", res);
+    } else {
+        let res: Vec<&str> = splitter::StrSplit::new(&input, delimiter).collect();
+        println!("{:?}", res);
+    }
 }
